ad2b21d444a7552d8bde0b70143915f2782a5b37
Christian Haser <chaser@Christians-MBP.lan>
2020-11-28 16:40:09 +0100

    Added Shelly 1l

---
+++ src/Devices/Shelly1l.ts
@@ -0,0 +1,55 @@
+import { IOnOff } from "../interfaces/Traits/IOnOff";
+import { IDevice } from "../interfaces/DeviceTypes/IDevice";
+import { DeviceType } from "../Enums/DeviceType";
+import { MQTTHandler } from "../MQTTHandler";
+import { Trait } from "../Enums/Trait";
+
+export class Shelly1l implements IDevice, IOnOff {
+	Name: string;
+	_id: string;
+	Type: DeviceType = DeviceType.Lights;
+	Status: boolean = false;
+	Traits: Trait[] = [Trait.OnOff];
+
+	mh: MQTTHandler;
+	shellyDevId: string;
+
+	//Defaults
+	aog_Type: string = 'action.devices.types.LIGHT';
+	aog_Traits: string[] = ['action.devices.traits.OnOff'];
+	aog_Attributes: any = {};
+
+	constructor(Name: string, id: string, mh: MQTTHandler) {
+		this.Name = Name;
+		this._id = id;
+		this.mh = mh;
+
+	}
+	Run(): void {
+		this.mh.Subscribe(`shellies/shelly1l-${this.shellyDevId}/relay/0`, this.onMQTT.bind(this))
+	}
+
+	onMQTT(payload: string) {
+		let tmpStatus = false;
+		if (payload.toString() == 'on') tmpStatus = true;
+
+		if (tmpStatus !== this.Status) {
+			this.Status = tmpStatus;
+			global.eventHandler.fire('change', this);
+		}
+	}
+ 
+	TurnOn(): void {
+		this.Status = true;
+		this.mh.SendCommand(`shellies/shelly1l-${this.shellyDevId}/relay/0/command`, 'on');
+		global.eventHandler.fire('change', this);
+	}
+	TurnOff(): void {
+		this.Status = false;
+		this.mh.SendCommand(`shellies/shelly1l-${this.shellyDevId}/relay/0/command`, 'off');
+		global.eventHandler.fire('change', this);
+	}
+	Toggle(): void {
+		this.Status ? this.TurnOff() : this.TurnOn();
+	}
+}
