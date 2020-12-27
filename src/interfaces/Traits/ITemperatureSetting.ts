ba0080f6ac6c471539dc7a123a95911ec27c9b96
Christian Haser <jerry.hermy@gmail.com>
2020-12-27 21:50:34 +0100

    Added an "AutomationTemperature" property to ITemperatureSetting

--- src/interfaces/Traits/ITemperatureSetting.ts
+++ src/interfaces/Traits/ITemperatureSetting.ts
@@ -2,6 +2,7 @@ import { Device } from "../../Device";
 
 export interface ITemperatureSetting extends Device {
 	TargetTemperature: number;
+	AutomationTemperature: number;
 
 	SetTemperature(value: number): void;
 	IncreaseTemperature(): void;
